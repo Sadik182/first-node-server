bd49624dedea2bf95ecb9440e8bf49fb502f3776
sadik182 <sadikurrahman728@gmail.com>
2022-10-20 19:08:55 +0600

    Server running

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+const users = [
+    {id: 1, name: 'Md. Sadikur Rahman', email: 'Sadik@gmail.com', phone: 01715925172},
+    {id: 2, name: 'Md. Shakil Khan', email: 'Shakil@gmail.com', phone: 01815925169},
+    {id: 3, name: 'Md. Kawsar Rahman', email: 'Kawsar@gmail.com', phone: 01615925185},
+]
+
+app.get('/', (req, res) => {
+    res.send('Hey This is From First Node Server')
+});
+
+app.get('/users', (req, res) => {
+    res.send(users);
+});
+
+app.get('/user/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log(id);
+    const user = users.find(uid => uid.id === id);
+    res.send(user);
+})
+
+app.listen(port, () => {
+    console.log('Server is Running on port', port);
+})
